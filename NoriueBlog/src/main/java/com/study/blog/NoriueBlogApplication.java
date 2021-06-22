ea9245f51b9d5815cf0d7c832a0c1e4919b027a1
Beom <Beom@192.168.1.3>
2021-06-22 19:06:05 +0900

    First commit

---
+++ NoriueBlog/src/main/java/com/study/blog/NoriueBlogApplication.java
@@ -0,0 +1,13 @@
+package com.study.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoriueBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoriueBlogApplication.class, args);
+	}
+
+}
